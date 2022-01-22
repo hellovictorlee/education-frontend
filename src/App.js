fa2327a930bd5a7dada734367213f7ac99f42207
Victor Lee <victory2152@livemail.tw>
2022-01-22 11:18:15 +0800

    update

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
 function App() {
   return (
-    <div>
-      <script
-        type="text/javascript"
-        src="//cdn.datacamp.com/dcl/lastest/dcl-react.js.gz"
-      ></script>
-      <div data-datacamp-exercise data-lang="python">
-        <code data-type="pre-exercise-code"># no pec</code>
-        <code data-type="sample-code">
-          # Create a variable a, equal to 5 # Print out a
-        </code>
-        <code data-type="solution">
-          # Create a variable a, equal to 5 a = 5 # Print out a print(a)
-        </code>
-        <code data-type="sct">
-          test_object("a") test_function("print") success_msg("Great job!")
-        </code>
-        <div data-type="hint">
-          Use the assignment operator (<code>=</code>) to create the variable{" "}
-          <code>a</code>.
-        </div>
+    <div data-datacamp-exercise data-lang="python">
+      <code data-type="pre-exercise-code"># no pec</code>
+      <code data-type="sample-code">
+        # Create a variable a, equal to 5 # Print out a
+      </code>
+      <code data-type="solution">
+        # Create a variable a, equal to 5 a = 5 # Print out a print(a)
+      </code>
+      <code data-type="sct">
+        test_object("a") test_function("print") success_msg("Great job!")
+      </code>
+      <div data-type="hint">
+        Use the assignment operator (<code>=</code>) to create the variable{" "}
+        <code>a</code>.
       </div>
     </div>
   );
