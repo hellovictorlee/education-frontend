06e768b8b11873dc798fbd7f626f64e1371b84ee
Victor Lee <victory2152@livemail.tw>
2022-01-22 22:58:32 +0800

    update

--- src/App.js
+++ src/App.js
@@ -1,20 +1,15 @@
 function App() {
   return (
-    <div data-datacamp-exercise data-lang="python">
-      <code data-type="pre-exercise-code"># no pec</code>
-      <code data-type="sample-code">
-        # Create a variable a, equal to 5 # Print out a
-      </code>
-      <code data-type="solution">
-        # Create a variable a, equal to 5 a = 5 # Print out a print(a)
-      </code>
-      <code data-type="sct">
-        test_object("a") test_function("print") success_msg("Great job!")
-      </code>
-      <div data-type="hint">
-        Use the assignment operator (<code>=</code>) to create the variable{" "}
-        <code>a</code>.
-      </div>
+    <div
+      style={{ margin: "50px" }}
+      data-datacamp-exercise
+      data-lang="python"
+      data-height="auto"
+    >
+      <code data-type="pre-exercise-code"></code>
+      <code data-type="sample-code"></code>
+      <code data-type="solution"></code>
+      <code data-type="sct"></code>
     </div>
   );
 }
